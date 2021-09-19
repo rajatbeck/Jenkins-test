34dd32cb6ec55bd65610933eae3ab47c2e81c60f
rajatbeck <rajatmarcelb@gmail.com>
2021-09-19 15:04:14 +0530

    text support for timestamp changed color pallet

--- app/src/main/java/com/carousellnews/ui/BindingAdapter.kt
+++ app/src/main/java/com/carousellnews/ui/BindingAdapter.kt
@@ -1,12 +1,15 @@
 package com.carousellnews.ui
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.carousellnews.R
+import com.carousellnews.domain.models.RelativeTime
+import com.carousellnews.domain.models.enums.Duration
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imgView: ImageView, imgUrl: String?){
@@ -27,5 +30,32 @@ fun setSpaceItemDecoration(recyclerView: RecyclerView, spaceInPx: Float) {
     if (spaceInPx != 0f) {
         val itemDecoration = SimpleItemDecoration(recyclerView.context, spaceInPx.toInt())
         recyclerView.addItemDecoration(itemDecoration)
+    }
+}
+
+@BindingAdapter("setTimeStamp")
+fun setTimeStamp(textView: TextView,relativeTime: RelativeTime){
+    textView.text = when(relativeTime.duration){
+        Duration.SECONDS -> {
+            textView.context.resources.getQuantityString(R.plurals.secondCount,relativeTime.value,relativeTime.value)
+        }
+        Duration.MINUTES -> {
+            textView.context.resources.getQuantityString(R.plurals.minuteCount,relativeTime.value,relativeTime.value)
+        }
+        Duration.HOURS -> {
+            textView.context.resources.getQuantityString(R.plurals.hourCount,relativeTime.value,relativeTime.value)
+        }
+        Duration.DAYS -> {
+            textView.context.resources.getQuantityString(R.plurals.dayCount,relativeTime.value,relativeTime.value)
+        }
+        Duration.WEEK -> {
+            textView.context.resources.getQuantityString(R.plurals.weekCount,relativeTime.value,relativeTime.value)
+        }
+        Duration.MONTHS -> {
+            textView.context.resources.getQuantityString(R.plurals.monthCount,relativeTime.value,relativeTime.value)
+        }
+        Duration.YEAR -> {
+            textView.context.resources.getQuantityString(R.plurals.yearCount,relativeTime.value,relativeTime.value)
+        }
     }
 }
